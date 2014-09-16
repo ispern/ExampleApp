d69f0b2f93cb83f5bc70a1f05ca11dcd161ae5b0
hiromitsu matsuoka <matsuoka@ispern.com>
2014-09-17 02:25:38 +0900

    edit circle.yml

--- tools/deploy/stg.rb
+++ tools/deploy/stg.rb
@@ -4,13 +4,13 @@ set :branch, 'deployment/staging'
 set :user, 'ec2-user'
 set :deploy_to, "/home/#{fetch(:user)}/workspace/#{fetch(:application)}"
 
-set :ssh_options, {
-  :keys => [File.join(ENV['HOME'], '.ssh', 'id_circleci_github'), File.join(ENV['HOME'], '.ssh', 'id_54.68.152.67')],
-    # keys: %w(tools/keys/vagrant.pem File.join(ENV['HOME'], '.ssh', 'github'))
-  :forward_agent => true,
-  :auth_methods => %w(publickey),
-  :keys_only => true
-}
+# set :ssh_options, {
+#   :keys => [File.join(ENV['HOME'], '.ssh', 'id_circleci_github'), File.join(ENV['HOME'], '.ssh', 'id_54.68.152.67')],
+#     # keys: %w(tools/keys/vagrant.pem File.join(ENV['HOME'], '.ssh', 'github'))
+#   :forward_agent => true,
+#   :auth_methods => %w(publickey),
+#   :keys_only => true
+# }
 
 server '54.68.152.67', user: 'ec2-user', roles: %w{web app}
 
