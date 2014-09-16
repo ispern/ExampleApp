33497b59be0f1628a1d6202c3ae46c0b6f7ac2b0
hiromitsu matsuoka <matsuoka@ispern.com>
2014-09-16 16:17:20 +0900

    edit ssh options

--- tools/deploy/stg.rb
+++ tools/deploy/stg.rb
@@ -4,12 +4,12 @@ set :branch, 'deployment/staging'
 set :user, 'ec2-user'
 set :deploy_to, "/home/#{fetch(:user)}/workspace/#{fetch(:application)}"
 
-set :ssh_options, {
-    keys: %w(tools/keys/vagrant.pem),
-    forward_agent: true,
-    auth_methods: %w(publickey),
-    keys_only: true
-}
+# set :ssh_options, {
+#     keys: %w(tools/keys/vagrant.pem),
+#     forward_agent: true,
+#     auth_methods: %w(publickey),
+#     keys_only: true
+# }
 
 server '54.68.152.67', user: 'ec2-user', roles: %w{web app}
 
