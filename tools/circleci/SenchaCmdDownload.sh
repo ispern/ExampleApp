0b8b05711d6e8911a691da333c6baa95665b9ed8
hiromitsu matsuoka <matsuoka@ispern.com>
2014-09-17 12:45:52 +0900

    update sencha cmd

--- tools/circleci/SenchaCmdDownload.sh
+++ tools/circleci/SenchaCmdDownload.sh
@@ -2,5 +2,5 @@
 if [ -d ~/bin/Sencha ]; then
     exit;
 fi;
-wget http://cdn.sencha.com/cmd/5.0.1.231/SenchaCmd-5.0.1.231-linux-x64.run.zip && unzip SenchaCmd-5.0.1.231-linux-x64.run.zip && chmod +x SenchaCmd-5.0.1.231-linux-x64.run
+chmod +x SenchaCmd-5.0.1.231-linux-x64.run
 ./SenchaCmdInstall.sh
